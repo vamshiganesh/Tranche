6758805d3729ffbd1b3cb2ad0cd41e8c28905d07
Vamshi Ganesh <vamshiganesh27@gmail.com>
2026-06-29 17:52:24 +0000

    Security config updated

--- src/main/java/com/tranche/common/config/SecurityConfig.java
+++ src/main/java/com/tranche/common/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> {})
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
