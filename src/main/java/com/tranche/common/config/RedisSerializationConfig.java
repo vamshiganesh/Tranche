00c52409a8b01eefdf17558f06504fb5a6f9e42b
Vamshi Ganesh <vamshiganesh27@gmail.com>
2026-06-29 12:05:19 +0000

    RedisSerilization Configured - 2

--- src/main/java/com/tranche/common/config/RedisSerializationConfig.java
+++ src/main/java/com/tranche/common/config/RedisSerializationConfig.java
@@ -1,7 +1,9 @@
 package com.tranche.common.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,11 @@ public class RedisSerializationConfig {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.activateDefaultTyping(
+                LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY
+        );
         return mapper;
     }
 
